f37862503f98a7eb9fec3baf59b5471c6d441682
Austin Comstock <austinjcomstock@gmail.com>
2023-11-23 10:53:29 -0700

    Added classNames, updated styling, button and input text

--- src/RandomGenerator.js
+++ src/RandomGenerator.js
@@ -7,19 +7,27 @@ export const RandomGenerator = () => {
 
   // generateRandom
   function generateRandom() {
-    console.log("button clicked");
+    // console.log("button clicked");
     // console.log(Math.floor(Math.random() * 15) + 1);
     setNumber(Math.floor(Math.random() * 15) + 1);
   }
 
   return (
     <div>
-      <h3>Random Generator</h3>
-      <button onClick={generateRandom}>Generate Movie</button>
+      <h2 className="Random-generatorh2">Random Generator</h2>
+      <button className="Random-generator-button" onClick={generateRandom}>
+        Click Here
+      </button>
       <br />
       <label>
-        Random Movie:{" "}
-        <input name="randomMovieInput" type="text" value={number} readOnly />
+        Movie Number:{" "}
+        <input
+          className="Random-generator-input"
+          name="randomMovieInput"
+          type="text"
+          value={number}
+          readOnly
+        />
       </label>
     </div>
   );
